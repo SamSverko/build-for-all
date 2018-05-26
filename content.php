5c0b48868fca4ea80a117f969a1b2e7168eee5e8
Sam Legros <email@samlegros.com>
2018-05-26 08:58:13 -0400

    Continue content page build

--- content.php
+++ content.php
@@ -2,18 +2,33 @@
 $pageTitle = "Content";
 require_once "_header.php";
 
-include_once "content/latest-article.php";
+include "content/latest-article.php"; // content-section-1
 ?>
 
 <hr class="contain-to-1000">
 
+<div class="container-fluid contain-to-1000 content-section-2">
+  <div class="row">
+    <div class="col-sm-3">
+      <p>BUSINESS</p>
+    </div>
+    <div class="col-sm-3">
+      <p>DESIGN</p>
+    </div>
+    <div class="col-sm-3">
+      <p>DEVELOPMENT</p>
+    </div>
+    <div class="col-sm-3">
+      <p>STORIES</p>
+    </div>
+  </div>
+</div>
+
+<hr class="contain-to-1000" style="margin-top: 30px;">
+
 <div class="container-fluid" style="max-width: 700px;">
   <?php include "content/content.1.php"; ?>
 </div>
-
-
-
-
 
 <?php
 require_once "_footer.php";
--- index.php
+++ index.php
@@ -26,28 +26,7 @@ require_once "_header.php";
   </div>
 </div>
 
-<!-- <div class="container-fluid contain-to-1000 index-section-2">
-  <h1 tabindex="0" class="font-bold text-center">Latest article</h1>
-  <div class="row">
-    <div class="col-sm-6 background-light-blue" id="index-section-2-img">
-    </div>
-    <div class="col-sm-6">
-      <p tabindex="0" class="font-bold">A new article about accessible SEO</p>
-      <p tabindex="0" class="color-grey">Easily configure your website for top rankings, among other best practices that make your website more accessible.</p>
-      <div class="row">
-        <div class="col-3 text-center">
-          <img tabindex="0" class="rounded-circle" src="img/image_katlynn.jpg" alt="Katlynn Sverko headshot">
-        </div>
-        <div class="col-9 index-section-2-author">
-          <p tabindex="0" class="color-grey">Katlynn Sverko</p>
-          <p tabindex="0" class="color-grey">May 21, 2018</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
-
-<?php include_once __DIR__ .  "/content/latest-article.php"; ?>
+<?php include __DIR__ .  "/content/latest-article.php"; ?>
 
 <hr class="contain-to-1000">
 
